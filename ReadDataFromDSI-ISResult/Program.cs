4a32d4df63c902b9acfef6eebfae891a20312f69
Kjartan Birgisson <kjartan@stubbur.com>
2020-10-04 10:06:59 +0000

    Add project files.

---
+++ ReadDataFromDSI-ISResult/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadDataFromDSI_ISResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
